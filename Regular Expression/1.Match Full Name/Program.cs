9f538d97d811bd80a42c3cc4377b43aef81dab73
FilipYanchev <129623815+FilipYanchev@users.noreply.github.com>
2023-11-16 13:19:03 +0000

    Add project files.

---
+++ Regular Expression/1.Match Full Name/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.RegularExpressions;
+
+string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+";
+Regex regex = new Regex(pattern);
+
+string input = Console.ReadLine();
+
+MatchCollection mathes = regex.Matches(input);
+
+Console.WriteLine(string.Join(" ", mathes));
---
+++ Regular Expression/3.Match Dates/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+string pattern = @"\b(?<day>\d{2})(?<sep>[\.\-\/])(?<month>[A-Z][a-z]{2})\k<sep>(?<year>\d{4})\b";
+Regex regex = new Regex(pattern);
+
+string input = Console.ReadLine();
+
+MatchCollection mathes = regex.Matches(input);
+
+foreach (Match match in mathes)
+{
+    Console.WriteLine($"Day: {match.Groups["day"]}, Month: {match.Groups["month"]}, Year: {match.Groups["year"]}");
+}
