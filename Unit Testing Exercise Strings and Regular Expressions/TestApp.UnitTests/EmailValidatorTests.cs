2b30c4ba5ec778c8b7f581767d2b8114b82ab3c0
FilipYanchev <129623815+FilipYanchev@users.noreply.github.com>
2023-11-22 13:58:06 +0000

    Add project files

---
+++ Unit Testing Exercise Strings and Regular Expressions/TestApp.UnitTests/EmailValidatorTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class EmailValidatorTests
+{
+   
+    [TestCase("fili.v.yanchev@gmail.com")]
+    [TestCase("filipyanchev@students.softuni.bg")]
+    [TestCase("fvy2180@gmail.com")]
+    public void Test_ValidEmails_ReturnsTrue(string email)
+    {
+        // Arrange
+
+
+        // Act
+        bool result = EmailValidator.IsValidEmail(email);
+
+        // Assert
+        Assert.That(result, Is.True);
+    }
+
+    
+    [TestCase("invalid mail@valid.domain")]
+    [TestCase("valid%mail@in_valid.domain")]
+    [TestCase("invakid/mail@invalid.d")]
+    public void Test_InvalidEmails_ReturnsFalse(string email)
+    {
+        // Arrange
+
+        // Act
+        bool result = EmailValidator.IsValidEmail(email);
+        
+        // Assert
+        Assert.That(result, Is.False);
+    }
+}
